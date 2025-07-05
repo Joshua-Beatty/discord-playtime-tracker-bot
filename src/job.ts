413cc6480e8eab2d894d4b057cb37a7a2cb8d904
Joshua Beatty <joshuatb6@gmail.com>
2025-07-04 22:34:48 -0600

    slight refactoring

--- src/job.ts
+++ src/job.ts
@@ -12,15 +12,18 @@ export default async function job(
     const Guilds = await client.guilds.fetch()
     let userCount = 0
     logger.info(`Fetcing guild users of ${Guilds.size} guilds`)
+    const values: (typeof activities.$inferInsert)[] = []
     for (const [gid, partialGuild] of Guilds) {
         const guild = await partialGuild.fetch()
         const users = await guild.members.fetch({ withPresences: true })
-        const values: (typeof activities.$inferInsert)[] = []
         for (const [uid, user] of users) {
             if (user.user.bot) continue
             userCount++
             for (const activity of user.presence?.activities || []) {
-                if (activity.applicationId && activity.type == ActivityType.Playing) {
+                if (
+                    activity.applicationId &&
+                    activity.type == ActivityType.Playing
+                ) {
                     values.push({
                         guildId: gid,
                         userId: uid,
@@ -33,25 +36,25 @@ export default async function job(
                 }
             }
         }
-        logger.info(`Inserting/Updating ${values.length} row(s)`)
-        if (values.length)
-            await db
-                .insert(activities)
-                .values(values)
-                .onConflictDoUpdate({
-                    target: [
-                        activities.guildId,
-                        activities.userId,
-                        activities.applicationId,
-                    ],
-                    set: {
-                        minutes: sql`minutes + 1`,
-                        applicationName: sql`excluded.applicationName`,
-                        lastPlayed: sql`excluded.lastPlayed`,
-                    },
-                })
-        logger.info(
-            `Fetched ${userCount} user(s) from ${Guilds.size} guild(s). Found ${values.length} activities.`
-        )
     }
+    logger.info(`Inserting/Updating ${values.length} row(s)`)
+    if (values.length)
+        await db
+            .insert(activities)
+            .values(values)
+            .onConflictDoUpdate({
+                target: [
+                    activities.guildId,
+                    activities.userId,
+                    activities.applicationId,
+                ],
+                set: {
+                    minutes: sql`minutes + 1`,
+                    applicationName: sql`excluded.applicationName`,
+                    lastPlayed: sql`excluded.lastPlayed`,
+                },
+            })
+    logger.info(
+        `Fetched ${userCount} user(s) from ${Guilds.size} guild(s). Found ${values.length} activities.`
+    )
 }
